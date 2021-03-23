3b5081d092cf5a3f63a923e750e1a12359644c9a
banmiseon <jjaq88777@hanmail.net>
2021-03-23 15:02:31 +0900

    ㅈㅂㅈㅂ

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './docs'),
-    publicPath: '/vuetodo/docs/',
+    publicPath: './vuetodo/',
     filename: 'build.js'
   },
   module: {
