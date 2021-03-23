d524e9ff641b2f203432572b15d704592768b9cf
banmiseon <jjaq88777@hanmail.net>
2021-03-23 14:37:10 +0900

    멍충이

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './docs'),
-    publicPath: 'https://github.com/sahwasa/vuetodo/',
+    publicPath: 'https://sahwasa.github.io/vuetodo/',
     filename: 'build.js'
   },
   module: {
