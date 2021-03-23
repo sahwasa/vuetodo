fd841d3fced4eb9433d8cccba117482ed494d190
banmiseon <jjaq88777@hanmail.net>
2021-03-23 14:20:15 +0900

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ var webpack = require('webpack')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    path: path.resolve(__dirname, './docs'),
+    publicPath: 'https://github.com/sahwasa/vuetodo/',
     filename: 'build.js'
   },
   module: {
