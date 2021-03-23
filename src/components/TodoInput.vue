e13a2a2d3c40d7e8f9973dce348c94746ab06c99
banmiseon <jjaq88777@hanmail.net>
2021-03-23 14:12:17 +0900

    first

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">  
+    <span  class="addContainer" v-on:click='addTodo'>
+      <i class="addBtn fa fa-plus" area-hidden="true"></i>
+    </span>
+
+    <modal v-if="showModal" @close="showModal=false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal=false">
+        할 일을 입력하세요.
+        <i class="closeModalBtn fa fa-times" area-hidden="true"></i>
+      </span>
+
+    </modal>
+  </div>
+</template>
+
+<script>
+import Modal from './common/Modal.vue'
+
+export default {
+  props:['propsdata'],
+  data(){
+    return {
+      newTodoItem : '',
+      showModal:false
+    }
+  },
+  methods:{
+    addTodo(){
+      if(this.newTodoItem !== ''){
+        var value=this.newTodoItem && this.newTodoItem.trim();
+        this.$emit('addTodo',value);
+        this.clearInput();
+      }else{
+        this.showModal = !this.showModal;
+      }
+    },
+    clearInput(){
+      this.newTodoItem='';
+    }
+  },
+  components:{
+    Modal : Modal
+  }
+}
+</script>
+
+<style scoped>
+input:focus{outline:none;}
+.inputBox{background-color:#fff;height:50px;line-height:50px;border-radius:5px;}
+.inputBox input{border-style:none;font-size:0.9rem;text-align:left;}
+.addContainer{float:right;background:linear-gradient(to right, #6478fb, #8763fb);display:block;width:3rem;border-radius:0 5px 5px 0;}
+.addBtn{color:#fff;vertical-align:middle;}
+</style>
